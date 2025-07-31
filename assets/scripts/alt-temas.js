7999435563597ffdfd73a09f807b3046e938bf7b
Gustavo Tamanini <gustavotamanini2005@gmail.com>
2025-07-31 15:17:32 -0300

    Botão de alternar temas criado.
    Salvando os arquivos antes de alterar boa parte das CSS...

---
+++ assets/scripts/alt-temas.js
@@ -0,0 +1,10 @@
+cxAltTema = document.querySelector('#alternar-tema');
+const body = document.body;
+cxAltTema.addEventListener('mouseover', function(e){
+    
+})
+
+cxAltTema.addEventListener('click', function(e){
+    //body.classList.add('modo-escuro')
+    body.classList.toggle('modo-escuro')
+})
