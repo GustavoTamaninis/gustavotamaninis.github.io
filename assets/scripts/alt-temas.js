6e52707619d53d9476612a3e5ba20e37cf0b122a
Gustavo Tamanini <gustavotamanini2005@gmail.com>
2025-07-31 16:23:10 -0300

    Alternância de temas feito.
    Ainda me falta guardar o tema no localStorage(), mas de resto, tranquilo.

--- assets/scripts/alt-temas.js
+++ assets/scripts/alt-temas.js
@@ -1,8 +1,5 @@
 cxAltTema = document.querySelector('#alternar-tema');
 const body = document.body;
-cxAltTema.addEventListener('mouseover', function(e){
-    
-})
 
 cxAltTema.addEventListener('click', function(e){
     //body.classList.add('modo-escuro')
