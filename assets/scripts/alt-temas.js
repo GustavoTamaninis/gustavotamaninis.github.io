632044fdce680cd3b923387acf2593e860cefd7c
Gustavo Tamanini <gustavotamanini2005@gmail.com>
2025-08-04 21:36:50 -0300

    localStorage() adicionado ao tema da página.

--- assets/scripts/alt-temas.js
+++ assets/scripts/alt-temas.js
@@ -4,4 +4,17 @@ const body = document.body;
 cxAltTema.addEventListener('click', function(e){
     //body.classList.add('modo-escuro')
     body.classList.toggle('modo-escuro')
+    if(body.classList.contains('modo-escuro')){
+        localStorage.setItem('tema', 'escuro');
+    }else{
+        localStorage.removeItem('tema');
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    temaSalvo = localStorage.getItem('tema');
+
+    if(temaSalvo === 'escuro'){
+        body.classList.add('modo-escuro')
+    }
 })
